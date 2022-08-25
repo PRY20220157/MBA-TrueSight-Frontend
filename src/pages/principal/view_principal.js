76d583c3c82a5d30cfa4b18892074d8aa0e87a21
giusencito <66499088+giusencito@users.noreply.github.com>
2022-08-24 21:41:21 -0500

    MBA_Cambios_Visuales

--- src/pages/principal/view_principal.js
+++ src/pages/principal/view_principal.js
@@ -1,29 +1,52 @@
 import Grid from "@mui/material/Grid";
 import {Navigate, useNavigate} from "react-router";
 import usePrincipal from "../../business/principal";
-
+import predictionimage from './images/PredictionImage.png'
+import historialimage from './images/HistorialImage.png'
+import tutorialimage from './images/TutorialImage.png'
+import optionimage from './images/OptionImage.png'
+import './principal.css'
+import { Typography } from "@mui/material";
 export const ViewPrincipal = props => {
 
     const hook = usePrincipal()
 
     return (
         <>
+                 <Typography variant="subtitle1">Bienvenido</Typography>
+                   &nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;&nbsp;
+                    
             <Grid container spacing={2} style={{cursor:'pointer', minHeight:'100% !important'}}>
                 <Grid item xs={12} onClick={(e) => hook.goToPrediction()}
-                      sx={{backgroundColor:"green"}}>
-                    Prediccion
+                      >
+                    
+                    <img className="predictionimage"
+                      src={predictionimage}
+                      alt="predictionimage"
+                    ></img>
                 </Grid>
                 <Grid item xs={12} onClick={(e) => hook.goToHistory()}
-                      sx={{backgroundColor:"red"}}>
-                    Historial
+                >
+                     <img className="historialimage"
+                     src={historialimage}
+                     alt="historialimage"
+                   ></img>
                 </Grid>
                 <Grid item xs={8} onClick={(e) => hook.goToTutorial()}
-                      sx={{backgroundColor:"yellow"}}>
-                    Tutorial
+                      >
+                    
+                    <img className="tutorialimage"
+                     src={tutorialimage}
+                     alt="tutorialimage"
+                   ></img>
                 </Grid>
                 <Grid item xs={4} onClick={(e) => hook.goToOptions()}
-                      sx={{backgroundColor:"blue"}}>
-                    Opciones
+                     >
+                  <img className="optionimage"
+                     src={optionimage}
+                     alt="optionimage"
+                   ></img>
                 </Grid>
             </Grid>
         </>
