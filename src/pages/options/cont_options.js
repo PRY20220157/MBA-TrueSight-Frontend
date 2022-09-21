53196bc4f69ef0c837b5ca2dc471110cf2d255a8
yperezbe <yperezbe@emeal.nttdata.com>
2022-09-20 22:10:59 -0500

    fix scroll options view

--- src/pages/options/cont_options.js
+++ src/pages/options/cont_options.js
@@ -1,25 +1,17 @@
 import {ViewOptions} from "./view_options";
 import './cont_option.css'
-import { Box, Container } from "@mui/material";
-import { CompCoverPage } from "comps/comp_cover_page";
-import { ViewTutorial } from "pages/tutorial/view_tutorial";
+import {Box, Container} from "@mui/material";
+import {CompCoverPage} from "comps/comp_cover_page";
+import {ViewTutorial} from "pages/tutorial/view_tutorial";
+
 export const ContOptions = props => {
-    return(
-        <div className="options-container" >
-    
-    <CompCoverPage>
-                    
-    
-                       <ViewOptions/>
-                       
-        
-    
-        </CompCoverPage>
-    
-    
-    
+    return (
+        <div className="options-container">
+            <CompCoverPage>
+                <ViewOptions/>
+            </CompCoverPage>
         </div>
-        
-        
-        );
+
+
+    );
 }
--- src/pages/options/view_options.js
+++ src/pages/options/view_options.js
@@ -1,13 +1,21 @@
-import { Paper } from "@mui/material";
+import {Paper} from "@mui/material";
 import ReactPlayer from "react-player";
 import {CompCoverPage} from "../../comps/comp_cover_page";
+import Box from "@mui/material/Box";
 
 export const ViewOptions = props => {
-    return(<>
+    return (
+        <>
+            <Box sx={{
+                display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", flexGrow: 1,
+                ml: 1, mr: 1
+            }}>
+                <Paper sx={{width: "100%", borderRadius: 3, background: 'rgba(250, 250, 250, 0.95)'}}
+                       elevation={24}>
+                    a
+                </Paper>
+            </Box>
+        </>
 
-    <h1>Options
-    </h1>
-    </>
-    
     );
 }
