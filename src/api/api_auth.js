fc30ee55abd49a0e8d8a1f94694d4d14b5bd5744
yperezbe <yperezbe@emeal.nttdata.com>
2022-09-20 20:27:32 -0500

    fix

--- src/api/api_auth.js
+++ src/api/api_auth.js
@@ -1,15 +1,18 @@
 import * as conn from './connection';
 import {HD_AUTHORIZATION, OWL_MBA_TS, URL_TRUE_SIGHT_BACKEND} from "util/constants";
+import axios from "axios";
 
 const headers = {
     [HD_AUTHORIZATION]: localStorage.getItem(OWL_MBA_TS),
 }
-export const authenticate = (data) => {
+export const authenticate = async (data) => {
     let config = {
         url: URL_TRUE_SIGHT_BACKEND + 'auth/jwt/create/',
         method: 'post',
         headers,
         data
     }
+    let result = await axios(config).then();
+console.log(result)
     return conn.send(config);
 }
