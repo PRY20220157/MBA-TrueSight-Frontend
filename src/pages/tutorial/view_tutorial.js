6c29bd997022ff9e6f222df928f47fff99313050
yperezbe <yperezbe@emeal.nttdata.com>
2022-09-30 14:06:20 -0500

    update tutorual video

--- src/pages/tutorial/view_tutorial.js
+++ src/pages/tutorial/view_tutorial.js
@@ -6,7 +6,7 @@ import { Paper ,Grid, Box} from "@mui/material";
 export const ViewTutorial = props => {
     return(<>
             <Paper  elevation={10} sx={{borderRadius:2,background: 'rgba(250, 250, 250, 0.8)',height:"40vh", width:"600px",m:3 }}>
-                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/Or5fsFq7IUs" frameBorder="0"
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/ofeRxClzxwo" frameBorder="0"
                         allowFullScreen></iframe>
 
             </Paper>
